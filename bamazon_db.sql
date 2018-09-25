b88ae56369510eb31b781711051c0d62d705e2f7
james inch <james.inch0103@gmail.com>
2018-09-24 22:36:35 -0400

    add gitignore

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -13,36 +13,18 @@ PRIMARY KEY (id)
 );
 
 INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Rusty spoons, 6-piece set", "Weapons", 15.00, 1000);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Katana(big sword)", "Weapons", 2000.00, 5);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("wakizashi(little sword)", "Weapons", 1500.00, 5);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Nunchaku", "Weapons", 165.00, 200);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Pickle Pairing Knife", "Weapons", 20.00, 1000);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Flintlock", "Weapons", 750.00, 66);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Blunderbuss", "Weapons", 4500.00, 50);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("shuriken(Throwing Stars)", "Weapons", 60.00, 10000);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Morning star(Club)", "Weapons", 110.00, 77);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("excalibur", "Weapons", 1000000.00, 1);
-
-INSERT INTO products (product, department, price, stock_quantity)
-VALUES ("Crossbow", "Weapons", 250.00, 666);
+VALUES 
+	("Rusty spoons, 6-piece set", "Weapons", 15.00, 1000),
+	("Katana(big sword)", "Weapons", 2000.00, 5),
+    ("wakizashi(little sword)", "Weapons", 1500.00, 5),
+	("Nunchaku", "Weapons", 165.00, 200),
+    ("Pickle Pairing Knife", "Weapons", 20.00, 1000),
+	("Flintlock", "Weapons", 750.00, 66),
+	("Blunderbuss", "Weapons", 4500.00, 50),
+	("shuriken(Throwing Stars)", "Weapons", 60.00, 10000),
+	("Morning star(Club)", "Weapons", 110.00, 77),
+	("excalibur", "Weapons", 1000000.00, 1),
+    ("Crossbow", "Weapons", 250.00, 666);
+	("Slingshot", "Weapons", 10.00, 350);
 
 SELECT * FROM products;
