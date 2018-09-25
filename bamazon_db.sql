eafad3d765edfc20063a8ba72103bfc722778d98
james inch <james.inch0103@gmail.com>
2018-09-25 00:31:33 -0400

    update

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -14,17 +14,17 @@ PRIMARY KEY (id)
 
 INSERT INTO products (product, department, price, stock_quantity)
 VALUES 
-	("Rusty spoons, 6-piece set", "Weapons", 15.00, 1000),
-	("Katana(big sword)", "Weapons", 2000.00, 5),
-    ("wakizashi(little sword)", "Weapons", 1500.00, 5),
-	("Nunchaku", "Weapons", 165.00, 200),
-    ("Pickle Pairing Knife", "Weapons", 20.00, 1000),
-	("Flintlock", "Weapons", 750.00, 66),
-	("Blunderbuss", "Weapons", 4500.00, 50),
-	("shuriken(Throwing Stars)", "Weapons", 60.00, 10000),
-	("Morning star(Club)", "Weapons", 110.00, 77),
-	("excalibur", "Weapons", 1000000.00, 1),
-    ("Crossbow", "Weapons", 250.00, 666);
-	("Slingshot", "Weapons", 10.00, 350);
+	("Rusty spoons, 6-piece set", "Weapons", 15.50, 1000),
+	("Katana(big sword)", "Weapons", 2000.75, 5),
+    ("wakizashi(little sword)", "Weapons", 1500.99, 5),
+	("Nunchaku", "Weapons", 165.99, 200),
+    ("Pickle Pairing Knife", "Weapons", 20.99, 1000),
+	("Flintlock", "Weapons", 750.99, 66),
+	("Blunderbuss", "Weapons", 4500.99, 50),
+	("shuriken(Throwing Stars)", "Weapons", 60.99, 10000),
+	("Morning star(Club)", "Weapons", 110.99, 77),
+	("excalibur", "Weapons", 1000000.99, 1),
+    ("Crossbow", "Weapons", 250.99, 666);
+	("Slingshot", "Weapons", 10.99, 350);
 
 SELECT * FROM products;
